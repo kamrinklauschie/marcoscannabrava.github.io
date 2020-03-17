d01b80eacd496b829b2fb6ef6b96b334bc723ad3
Marcos Cannabrava <mpcannabrava@gmail.com>
2020-03-17 10:21:09 -0300

    prune extra files

--- index.js
+++
@@ -1,5 +0,0 @@
-// fetch("./components/navbar.html")
-//   .then(response => response.text()).then(data => {
-//     document.querySelector("#header").innerHTML = data;
-//   });
-// document.getElementById("header").innerHTML = open("./components/navbar.html");
