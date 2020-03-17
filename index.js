6769af70719cfcc69b18d647a45f0ab99c301ba6
Marcos Cannabrava <mpcannabrava@gmail.com>
2020-03-17 10:16:37 -0300

    add project demos

---
+++ index.js
@@ -0,0 +1,5 @@
+// fetch("./components/navbar.html")
+//   .then(response => response.text()).then(data => {
+//     document.querySelector("#header").innerHTML = data;
+//   });
+// document.getElementById("header").innerHTML = open("./components/navbar.html");
